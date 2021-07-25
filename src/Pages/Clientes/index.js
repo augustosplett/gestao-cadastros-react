ce4778cbdf44cf9be21705af69493d22d94e1d84
augustosplett <augusto.splett@gmail.com>
2021-07-25 03:37:52 +0000

    adicionadas funções para inclusão de novos registros, deleção e edição

--- src/Pages/Clientes/index.js
+++ src/Pages/Clientes/index.js
@@ -4,6 +4,13 @@ import MaterialTable from "material-table";
 
 const Clientes = () => {
 
+    function guidGenerator() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }
+      //https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523
+
     const [repository, setRepository] = useState([]);
 
     useEffect(() => {
@@ -18,7 +25,7 @@ const Clientes = () => {
     },[repository]);
 
     const tableColumns = [
-        {title: 'Cod Cliente', field: 'id'},
+        {title: 'Cod Cliente', field: 'id', editable: "never"},
         {title: 'Nome', field: 'customer_name'},
         {title: 'Phone', field: 'phone_number'},
         {title: 'e-mail', field: 'email'},
@@ -36,11 +43,28 @@ const Clientes = () => {
                 options={{
                     search: false,
                     paging: false,
-                    tableLayout: "auto"
+                    tableLayout: "auto",
+                    addRowPosition: "first",
+                    actionsColumnIndex: -1
                 }}
                 editable={{
-                    onRowAdd: (newRow) => new Promise((resolve, reject)=>{
-                        const updatedRows = [...repository, newRow];
+                    onRowAdd: (newRow) => new Promise((resolve, reject) => {
+                        const updatedRows = [...repository, {id:guidGenerator(),...newRow}];
+                        setRepository(updatedRows);
+                        resolve();
+
+                    }),
+                    onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
+                        const index = selectedRow.tableData.id;
+                        const updatedRows = [...repository];
+                        updatedRows.splice(index, 1);
+                        setRepository(updatedRows);
+                        resolve();
+                    }),
+                    onRowUpdate: (updatedRow, oldRow) => new Promise((resolve, reject) => {
+                        const index = oldRow.tableData.id;
+                        const updatedRows = [...repository];
+                        updatedRows[index] = updatedRow;
                         setRepository(updatedRows);
                         resolve();
 
--- src/Pages/Produtos/index.js
+++ src/Pages/Produtos/index.js
@@ -5,6 +5,13 @@ import MaterialTable from "material-table";
 
 const Produtos = () => {
 
+    function guidGenerator() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }
+      //https://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid/2117523#2117523
+
     const [repository, setRepository] = useState([]);
 
     useEffect(() => {
@@ -19,7 +26,7 @@ const Produtos = () => {
     },[repository]);
 
     const tableColumns = [
-        {title: 'Cod Produto', field: 'id'},
+        {title: 'Cod Produto', field: 'id', editable: "never"},
         {title: 'Fabricande', field: 'manufacturer'},
         {title: 'Modelo', field: 'model'},
         {title: 'Preço', field: 'price'},
@@ -37,11 +44,28 @@ const Produtos = () => {
                 options={{
                     search: false,
                     paging: false,
-                    tableLayout: "auto"
+                    tableLayout: "auto",
+                    addRowPosition: "first",
+                    actionsColumnIndex: -1
                 }}
                 editable={{
                     onRowAdd: (newRow) => new Promise((resolve, reject)=>{
-                        const updatedRows = [...repository, newRow];
+                        const updatedRows = [...repository, {id:guidGenerator(),...newRow}];
+                        setRepository(updatedRows);
+                        resolve();
+
+                    }),
+                    onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
+                        const index = selectedRow.tableData.id;
+                        const updatedRows = [...repository];
+                        updatedRows.splice(index, 1);
+                        setRepository(updatedRows);
+                        resolve();
+                    }),
+                    onRowUpdate: (updatedRow, oldRow) => new Promise((resolve, reject) => {
+                        const index = oldRow.tableData.id;
+                        const updatedRows = [...repository];
+                        updatedRows[index] = updatedRow;
                         setRepository(updatedRows);
                         resolve();
 
